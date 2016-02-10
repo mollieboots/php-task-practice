40bca7ec448e60745b9d136db30a901942342783
Jason Awbrey and Mollie Bootsma <student@epicodus.com>
2016-02-09 16:02:38 -0800

    Add all views

--- app/app.php
+++ app/app.php
@@ -10,30 +10,23 @@
 
     $app = new Silex\Application();
 
-    $app->get("/", function(){
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views'
+    ));
 
-        $output = "";
-
-        foreach (Task::getAll() as $task) {
-            $output = $output . "<li>" . $task->getDesc() . "</li>";
-        }
-
-        return $output .= "<form action='/taskAdded' method='post'><label for='desc'>Enter a new task</label><input name='desc' id='desc'><button type='submit'>Submit</button></form><form action='/deleteTasks' method='post'><button type='submit'>Clear Tasks</button></form>";
+    $app->get("/", function() use ($app){
+        return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
     });
 
-    $app->post("/taskAdded", function(){
+    $app->post("/taskadded", function() use ($app){
         $task = new Task($_POST['desc']);
         $task->save();
-        $thisTask = $task->getDesc();
-        $output = "<h1>New task added!</h1><div>Your task: $thisTask</div><a href='/'>Go To List</a><form action='/deleteTasks' method='post'><button type='submit'>Clear Tasks</button></form>";
-
-        return $output;
+        return $app['twig']->render('taskadded.html.twig', array('newtask' => $task));
     });
 
-    $app->post("/deleteTasks", function() {
+    $app->post("/taskdeleted", function() use ($app) {
         Task::deleteAll();
-
-        return "<h1>List Cleared!</h1><a href='/'>Home</a>";
+        return $app['twig']->render('taskdeleted.html.twig');
     });
 
     return $app;
